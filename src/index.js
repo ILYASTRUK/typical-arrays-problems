5eba7f38d8bb7889677f8a4a9b33bde6a2a55845
Struk Ilya <devil-ii@mail.ru>
2020-10-03 14:41:45 +0300

    feat: add min, max, avr functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) === false || array.length === 0) {
+    return 0;
+  } else {
+    let max = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < array.length; i++) {
+        if (max > array[i]) {
+            max = array[i];
+        }
+    }    
+  return max;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) === false || array.length === 0) {
+    return 0;
+  } else {
+    let max = Number.MIN_SAFE_INTEGER;
+    for (let i = 0; i < array.length; i++) {
+      if (max < array[i]) {
+          max = array[i];
+      }
+  }    
+    return max;
+  }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) === false || array.length === 0) {
+    return 0;
+  } else {
+    let buf = 0;
+    let sum = 0;
+      for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        buf++;
+      }
+      return sum/buf;
+  }
 }
